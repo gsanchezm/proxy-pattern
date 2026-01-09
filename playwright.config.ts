a82616b411c88d722d2bb957683ec39491660e91
gilbertosanchez <gilsanchezts@outlook.com>
2026-01-09 12:28:52 -0600

    modified apiVersion config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,18 +3,5 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   use: {
     headless: false,
-    connectOptions: {
-      wsEndpoint: `wss://ondemand.${process.env.SAUCE_REGION}.saucelabs.com/playwright?capabilities=${encodeURIComponent(
-        JSON.stringify({
-          browserName: 'chromium',
-          browserVersion: 'latest',
-          platformName: 'Windows 11',
-          'sauce:options': {
-            name: 'Playwright Cucumber Test',
-            build: process.env.GITHUB_RUN_ID
-          }
-        })
-      )}`
-    }
   }  
 });
