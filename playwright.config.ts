5a870dcafeb56e0ec9cb5590b1cbb78f8cd9a829
gilbertosanchez <gilsanchezts@outlook.com>
2026-01-09 10:44:25 -0600

    modified config from saucectl

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,19 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   use: {
-    headless: false
-  }
+    headless: false,
+    connectOptions: {
+      wsEndpoint: `wss://ondemand.${process.env.SAUCE_REGION}.saucelabs.com/playwright?capabilities=${encodeURIComponent(
+        JSON.stringify({
+          browserName: 'chromium',
+          browserVersion: 'latest',
+          platformName: 'Windows 11',
+          'sauce:options': {
+            name: 'Playwright Cucumber Test',
+            build: process.env.GITHUB_RUN_ID
+          }
+        })
+      )}`
+    }
+  }  
 });
